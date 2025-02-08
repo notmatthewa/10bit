f5b3069abd0bcfd3bb695a40c9987b08f91775d4
notmatthewa <mattkirkanderson@gmail.com>
2025-02-07 21:14:45 -0600

    init commit

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Vue.js</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello Vue.js!'
+    }
+  }
+}
+</script>
