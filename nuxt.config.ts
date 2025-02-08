d63977883b3304fe69f8505cb3459ac81b46c0b2
notmatthewa <mattkirkanderson@gmail.com>
2025-02-07 21:31:19 -0600

    fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts'],
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   tailwindcss: {
     exposeConfig: true,
     viewer: true,
+  },
+  googleFonts: {
+    families: {
+      Audiowide: true,
+      "42dot Sans": true,
+    },
   },
 })
